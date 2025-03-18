7b8dabb83b5035f55791a8cad0bdd9326c974b00
Trophydecoder <trophydecoder@gmail.com>
2025-03-18 19:48:31 +0200

    added bg,sticky and changes colors

---
+++ script.js
@@ -0,0 +1,10 @@
+   // Open the side navigation
+   function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+// Close the side navigation
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
