f70662bc6664a68cb3b96199559469840cfb4723
Trophydecoder <trophydecoder@gmail.com>
2025-03-18 20:57:45 +0200

    added links

--- script.js
+++ script.js
@@ -7,4 +7,11 @@
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
-
+function downloadResume() {
+    const link = document.createElement('a');
+    link.href = 'https://drive.google.com/file/d/1-oewf-ecxdPxfHDFkzT1WKW3mXKLLBXe/view?usp=drive_link';  // Change this to your actual resume file URL
+    link.download = 'Sithembiso Mdawe CV.pdf'; // Change this to your preferred file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
